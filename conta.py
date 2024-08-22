1f829d8f147d17b19197e83009cd759732164ba1
BiriBerto <96660740+BiriBerto1@users.noreply.github.com>
2024-08-22 19:35:37 -0300

    conta.py

--- conta.py
+++ conta.py
@@ -1,44 +1,95 @@
-from conta import Conta
+class Conta:
+    def __init__(self, titular, senha, saldo=0):
+        self.titular = titular
+        self.__saldo = saldo
+        self.__senha = senha
 
-def login():
-    print("Bem vindo a sua conta bancaria.")
-    print('Selecione 1 para se registrar ou 2 para fazer login.')
-    opcao_login = int(input(''))
-    return opcao_login
+    def consultar_saldo(self):
+        print('='*30)
+        print(f'{self.titular} O saldo da sua conta é R${self.__saldo}')
+        print('='*30)
 
-def menu():
-    print('Selecione o serviço desejado:')
-    print('1 - Consultar saldo.')
-    print('2 - Depositar.')
-    print('3 - Sacar.')
-    print('4 - Alterar senha.')
-    print('5 - Para sair.')
-    opcao_menu = int(input(''))
-    return opcao_menu
+    def depositar(self):
+        valor_deposito = float(input('Digite o valor do deposito. '))
+        if self.validar_senha():
+            self.__saldo += valor_deposito
+            print('Deposito realizado com sucesso.')
+        else:
+            print('Senha incorreta.')
 
-def main():
-    conta = Conta('','')
-    opcao_login = login()
+    def sacar(self):
+        valor_saque = float(input('Digite o valor do saque. '))
+        if self.validar_senha():
+            if valor_saque > self.__saldo:
+                print('Saldo insuficiente.')
+            else:
+                self.__saldo -= valor_saque
+                print('Saque realizado com sucesso!')
+        else:
+            print('Senha incorreta.')
 
-    if opcao_login == 1:
-        conta.cadastrar()
-    elif opcao_login == 2:
-        retorno, conta = conta.login()
-        if retorno:
-            while True:
-                opcao_menu = menu()
-                if opcao_menu == 1:
-                    conta.consultar_saldo()
-                elif opcao_menu == 2:
-                    conta.depositar()
-                    conta.atualizar()
-                elif opcao_menu == 3:
-                    conta.sacar()
-                    conta.atualizar()
-                elif opcao_menu == 4:
-                    conta.alterar_senha()
-                    conta.atualizar()
-                elif opcao_menu == 5:
-                    break
+    def alterar_senha(self):
+        senha_digitada = input('Digite sua senha para verificação. ')
+        self.__senha = senha_digitada
 
-main()
+    def validar_senha(self):
+        tentativa = 3
+        # while tentativa > 0:
+        senha_digitada = input('Digite sua senha. ')
+        #     if senha_digitada == self.__senha:
+        #         print('Certo')
+        #         return True
+        #     print('Errado')
+        #     tentativa -= 1
+        # return False
+        with open('usuario.txt', 'r') as arquivo:
+            for linha in arquivo:
+                partes = linha.strip().split(', ')
+                senha_armazenada = partes[1].split(': ')[1]
+                if senha_digitada == senha_armazenada:
+                    print('Certo')
+                    return True
+                else:
+                    tentativa -= 1
+                    print(f'Errado {tentativa} tentativas restantes.')
+                    return False
+
+    def login(self):
+        usuario_digitado = input('Digite o usuario. ')
+        senha_digitada = input('Digite sua senha.')
+
+        with open('usuario.txt', 'r') as arquivo:
+            for linha in arquivo:
+                if linha.startswith(f'Titular: {usuario_digitado}'):
+                    partes = linha.strip().split(', ')
+                    senha_armazenada = partes[1].split(': ')[1]
+                    if senha_digitada == senha_armazenada:
+                        print('Login bem-sucedido!')
+                        return True, Conta(usuario_digitado, senha_digitada, float(partes[2].split(': ')[1]))
+                    else:
+                        print('Senha incorreta.')
+                        return False
+        print('Titular não encontrado.')
+        return False
+
+    def cadastrar(self):
+        titular_digitado = input('Digite o nome do titular da conta. ')
+        senha_digitada = input('Cadastre sua senha. ')
+        self.titular = titular_digitado
+        self.__senha = senha_digitada
+        print('Cadastrado com sucesso.')
+
+        with open('usuario.txt', 'a') as arquivo:
+            arquivo.write(f'Titular: {self.titular}, Senha: {self.__senha}, Saldo: {self.__saldo}\n')
+
+        return self.titular, self.__senha
+
+    def atualizar(self):
+        with open('usuario.txt', 'r') as arquivo:
+            linhas = arquivo.readlines()
+
+        with open('usuario.txt', 'w') as arquivo:
+            for linha in linhas:
+                if linha.startswith(f'Titular: {self.titular}'):
+                    linha = f'Titular: {self.titular}, Senha: {self.__senha}, Saldo: {self.__saldo}\n'
+                arquivo.write(linha)
